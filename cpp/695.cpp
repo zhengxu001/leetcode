89598783fa7f0482aa9480d80be8198f7daa2947
Zen <zhengxu5511@gmail.com>
2017-10-16 11:56:06 +0100

    add cpp file

---
+++ cpp/695.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int maxArea = 0;
+        for(int i=0; i<grid.size(); i++)
+            for(int j=0; j<grid[0].size(); j++) {
+                maxArea = max(areaOfIsland(grid, i, j), maxArea);
+            }
+        return maxArea;
+    }
+
+    int areaOfIsland(vector<vector<int>>& grid, int i, int j) {
+        if(i>=0 && i<grid.size() && j>=0 && j < grid[0].size() && grid[i][j] == 1){
+            grid[i][j] = 0;
+            return 1 + areaOfIsland(grid, i+1, j) + areaOfIsland(grid, i-1, j) + areaOfIsland(grid, i, j+1) + areaOfIsland(grid, i, j-1);
+        }
+        return 0;
+    }
+};
